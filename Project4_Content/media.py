1e38bc088a69f1adff28f0925a584d97b5fd9945
Joshua Bernhard <joshuabernhard@Joshuas-MacBook-Pro.local>
2017-07-19 08:55:55 -0700

    updated media.py to contain docstrings

--- Project4_Content/media.py
+++ Project4_Content/media.py
@@ -3,7 +3,12 @@ import webbrowser
 class Movie():
     def __init__(self, movie_title, story_line, poster_image_url, trailer_youtube_url):
         '''
-        INPUT:initialize our class with stuff
+        INPUT:initialize with -
+            movie_title: The title of your movie as a string
+            story_line: The storyline of your movie as a string
+            poster_image_url: url link to poster image as a string
+            trailer_youtube_url: url link to youtube trailer as a string
+        OUTPUT: None
         '''
         self.title = movie_title
         self.storyline = story_line
@@ -11,4 +16,9 @@ class Movie():
         self.trailer_youtube_url = trailer_youtube_url
 
     def web_trailer(self):
+        '''
+        INPUT: None
+        OUTPUT: None
+        This method will start the trailer in trailer_youtube_url that lives on the instantiated object of the class Movie
+        '''
         webbrowser.open(self.trailer_youtube_url)
